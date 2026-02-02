d1d7fa8e78778655cfc0d9af778574c0e1e36bc2
MARCO ANTÔNIO GRASSI <marco_grassi@estudante.sesisenai.org.br>
2026-02-02 18:45:17 -0300

    Cria a classe Usario

---
+++ src/Usuario.java
@@ -0,0 +1,36 @@
+public class Usuario
+{
+    private String nome;
+    private String cpf;
+    private double saldo;
+
+    public Usuario(String nome, String cpf, double saldo) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.saldo = saldo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
