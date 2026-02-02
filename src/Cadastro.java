ae6e14faf1f9c201e2d9dce5380498c1d5e66e90
MARCO ANTÔNIO GRASSI <marco_grassi@estudante.sesisenai.org.br>
2026-02-02 19:32:07 -0300

    Cria o metodo cadastro

--- src/Cadastro.java
+++ src/Cadastro.java
@@ -1,3 +1,23 @@
-public class Cadastro {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Cadastro
+{
+    ArrayList<Usuario> cadastros = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
+    public static void cadastro()
+    {
+        System.out.println(" -- Digite seu nome:");
+
+        String nome = sc.nextLine();
+
+        System.out.println(" -- Digite seu cpf:");
+
+        String cpf = sc.nextLine();
+
+        Usuario usuario = new Usuario();
+
+    }
 }
