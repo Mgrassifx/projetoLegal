a60c91c7a17f7cc8a60575f540b16b79b87dedf1
MARCO ANTÔNIO GRASSI <marco_grassi@estudante.sesisenai.org.br>
2026-02-02 19:15:54 -0300

    Cria o metodo menuPrincipal

--- src/Inicio.java
+++ src/Inicio.java
@@ -1,4 +1,49 @@
+import java.util.Scanner;
+
 public class Inicio
 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void MenuPricipal()
+    {
+        int opcao = 0;
+
+        while (opcao != 3)
+        {
+            System.out.printf(" === MENU PRICIPAL ===");
+            System.out.printf(" 1 - Cadastro");
+            System.out.printf(" 2 - Login ");
+            System.out.println(" 3 - Sair");
+
+            try {
+                opcao = sc.nextInt();
+
+                switch (opcao)
+                {
+                    case 1:
+                        Cadastro.cadastro();
+                    break;
+
+                    case 2:
+                        Login.login();
+                    break;
+
+                    case 3:
+                        System.out.println(" saindo do banco Marquinhos Maia");
+                    return;
+
+                    default:
+                        System.out.println(" voce digitou uma opcao errado, tente novamente");
+                    break;
+
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
 
 }
